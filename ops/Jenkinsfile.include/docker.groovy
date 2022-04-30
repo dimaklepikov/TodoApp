4e6759903d5d82113cbeed0261cf6d0395231b10
Dmitriy Klepikov <klepikovdima@gmail.com>
2022-04-30 14:38:37 +0300

    Updated pushing to Nexus repo in Jenkinsfile

--- ops/Jenkinsfile.include/docker.groovy
+++ ops/Jenkinsfile.include/docker.groovy
@@ -2,14 +2,9 @@ def buildAndUpload(String dockerfilePath) {
     withDockerRegistry([ credentialsId: "${REGISTRY_CREDENTIALS}", url: "http://" + "${REGISTRY}" ]) {
         wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
 
-            todoImage = docker.build("${DOCKER_IMAGE}",  "-f ${dockerfilePath} .")
-            // docker.push("${REGISTRY}", "${DOCKER_IMAGE}:latest")
+            docker.build("${DOCKER_IMAGE}",  "-f ${dockerfilePath} .")
             sh "docker tag ${DOCKER_IMAGE} ${REGISTRY}/${DOCKER_IMAGE}:latest"
             sh "docker push ${REGISTRY}/${DOCKER_IMAGE}:latest"
-            // def todoImage = "docker build -t ${DOCKER_IMAGE}:${env.VERSION} -f ${dockerfilePath} ."
-            // todoImage.push("${env.VERSION}, ")
-
-            // docker.build("${DOCKER_IMAGE}:${env.VERSION}", "--no-cache -f ${WORKSPACE}/${dockerfilePath} .").push()
         }
     }
 }
